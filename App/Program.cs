81a36738a49d2895a9bc5e8d4af93d107bf6d92c
Andrius Andrikonis <andrikonis.andrius@gmail.com>
2023-04-24 22:01:52 +0300

    IDP service

---
+++ App/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOrchardCms();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseOrchardCore();
+
+app.Run();
